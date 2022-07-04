e711ffac73c75d88dccf1b608e509bb491d1c8d6
Franciel Ferreira <72324148+FrancielFerreira@users.noreply.github.com>
2022-07-04 17:04:48 -0300

    slide-otimizado

--- slide-jQuery/3/slide.js
+++ slide-jQuery/3/slide.js
@@ -1,67 +1,37 @@
-const allSlides = $("[data-slide-item]");
-const firstSlide = $("[data-slide-item]").first();
-const lastSlide = $("[data-slide-item]").last();
+function slider(speed) {
+  const allSlides = $("[data-slide-item]"),
+        firstSlide = $("[data-slide-item]").first(),
+        lastSlide = $("[data-slide-item]").last();
+  let speedSlide = setInterval(playSlide, speed);
 
-allSlides.attr("data-slide-item", "hide");
-firstSlide.attr("data-slide-item", "show");
+  allSlides.attr("data-slide-item", "hide");
+  firstSlide.attr("data-slide-item", "show");
 
-function prevSlide(slideActive) {
-  let prevSlide = slideActive.prev();
-  prevSlide.length === 0 ? prevSlide = lastSlide : 0;
-  slideActive.attr("data-slide-item", "hide");
-  prevSlide.attr("data-slide-item", "show");
-}
-
-function nextSlide(slideActive) {
-  let nextSlide = slideActive.next();
-  nextSlide.length === 0 ? nextSlide = firstSlide : 0;
-  slideActive.attr("data-slide-item", "hide");
-  nextSlide.attr("data-slide-item", "show");
-}
-
-function playSlide() {
-  const btnPrevSlide = $(this).attr("class");
-  let slideActive = $('[data-slide-item="show"]');
-  btnPrevSlide === "btn-prev" ? prevSlide(slideActive) : nextSlide(slideActive);
-}
-
-$(".btn-prev").click(playSlide);
-$(".btn-next").click(playSlide);
-setInterval(playSlide, 2000);
-
-
-/* backup slide
-const slideList = $("[data-slide-list]");
-const allSlides = $("[data-slide-item]");
-const firstSlide = $("[data-slide-item]").first();
-const lastSlide = $("[data-slide-item]").last();
-
-allSlides.attr("data-slide-item", "hide");
-firstSlide.attr("data-slide-item", "show");
-
-function playSlide() {
-  const btnPrevSlide = $(this).attr("class");
-  let slideActive = $('[data-slide-item="show"]');
-
-  if (btnPrevSlide === "btn-prev") {
+  function prevSlide(slideActive) {
     let prevSlide = slideActive.prev();
-    if (prevSlide.length === 0) {
-      prevSlide = lastSlide;
-    }
+    prevSlide.length === 0 ? prevSlide = lastSlide : 0;
     slideActive.attr("data-slide-item", "hide");
     prevSlide.attr("data-slide-item", "show");
-  } else {
+    clearInterval(speedSlide);
+    speedSlide = setInterval(playSlide, speed);
+  }
+
+  function nextSlide(slideActive) {
     let nextSlide = slideActive.next();
-    if (nextSlide.length === 0) {
-      nextSlide = firstSlide;
-    }
+    nextSlide.length === 0 ? nextSlide = firstSlide : 0;
     slideActive.attr("data-slide-item", "hide");
     nextSlide.attr("data-slide-item", "show");
+    clearInterval(speedSlide);
+    speedSlide = setInterval(playSlide, speed);
   }
+
+  function playSlide() {
+    const btnClick = $(this).attr("class");
+    let slideActive = $('[data-slide-item="show"]');
+    btnClick === "btn-prev" ? prevSlide(slideActive) : nextSlide(slideActive);
+  }
+
+  $(".btn-prev").click(playSlide);
+  $(".btn-next").click(playSlide);
 }
-
-$(".btn-prev").click(playSlide);
-$(".btn-next").click(playSlide);
-setInterval(playSlide, 2000);
-
-*/
+slider(5000)
